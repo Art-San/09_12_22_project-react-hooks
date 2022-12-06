0af3acd6f678d241f043f54fae1e6454e0191d42
Aleksander <asava@sibmail.com>
2022-12-06 22:53:03 +0700

    2. useRef()

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,12 +1,14 @@
 import React, { useRef, useState, useEffect } from 'react'
 import CardWrapper from '../../common/Card'
-import Divider from '../../common/Divider'
+// import Divider from '../../common/Divider'
 import SmallTitle from '../../common/typografy/SmallTitle'
 const PrevStateExample = () => {
     const prevState = useRef('')
     const [otherState, setOtherState] = useState('false')
     const toggleOtherState = () => {
-        setOtherState((prevState) => (prevState === 'false' ? 'true' : 'false'))
+        setOtherState((prevState) =>
+            prevState === 'false' ? 'true' : 'false'
+        )
     }
     useEffect(() => {
         prevState.current = otherState
@@ -14,14 +16,31 @@ const PrevStateExample = () => {
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
-            <Divider />
-            <p>prev State:{prevState.current}</p>
-            <p>Current State:{otherState}</p>
-            <button className="btn btn-primary" onClick={toggleOtherState}>
-                Toggle other state
-            </button>
+            <p>prev State: {prevState.current}</p>
+            <p>Corrent State: {otherState}</p>
+            <button className='btn btn-primary' onClick={toggleOtherState}>жми</button>
         </CardWrapper>
     )
 }
 
 export default PrevStateExample
+
+// const prevState = useRef('')
+// const [otherState, setOtherState] = useState('false')
+// const toggleOtherState = () => {
+//     setOtherState((prevState) => (prevState === 'false' ? 'true' : 'false'))
+// }
+// useEffect(() => {
+//     prevState.current = otherState
+// }, [otherState])
+// return (
+//     <CardWrapper>
+//         <SmallTitle>Предыдущее состояние</SmallTitle>
+//         <Divider />
+//         <p>prev State:{prevState.current}</p>
+//         <p>Current State:{otherState}</p>
+//         <button className="btn btn-primary" onClick={toggleOtherState}>
+//             Toggle other state
+//         </button>
+//     </CardWrapper>
+// )
--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react'
 import CardWrapper from '../../common/Card'
-import Divider from '../../common/Divider'
+// import Divider from '../../common/Divider'
 import SmallTitle from '../../common/typografy/SmallTitle'
 const RenderCountExample = () => {
     const renderCount = useRef(0)
@@ -14,13 +14,31 @@ const RenderCountExample = () => {
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
-            <Divider />
-            <p>render count:{renderCount.current}</p>
-            <button className="btn btn-primary" onClick={toggleOtherState}>
-                Toggle other state
-            </button>
+            <p>render count: {renderCount.current}</p>
+            <button className='btn btn-primary' onClick={toggleOtherState}>жми</button>
         </CardWrapper>
     )
 }
 
 export default RenderCountExample
+
+// const RenderCountExample = () => {
+//     const renderCount = useRef(0)
+//     const [otherState, setOtherState] = useState(false)
+//     const toggleOtherState = () => {
+//         setOtherState(!otherState)
+//     }
+//     useEffect(() => {
+//         renderCount.current++
+//     })
+//     return (
+//         <CardWrapper>
+//             <SmallTitle>Подсчет количества рендеров</SmallTitle>
+//             <Divider />
+//             <p>render count:{renderCount.current}</p>
+//             <button className="btn btn-primary" onClick={toggleOtherState}>
+//                 Toggle other state
+//             </button>
+//         </CardWrapper>
+//     )
+// }
