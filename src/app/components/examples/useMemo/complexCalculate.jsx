8df054c53f0bd3e39af935556a8c9d153cb15c84
Aleksander <asava@sibmail.com>
2022-12-09 14:28:40 +0700

    3. useMemo()

--- src/app/components/examples/useMemo/complexCalculate.jsx
+++ src/app/components/examples/useMemo/complexCalculate.jsx
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import CardWrapper from '../../common/Card'
 import SmallTitle from '../../common/typografy/SmallTitle'
 
 function factorial(n) {
     return n ? n * factorial(n - 1) : 1
 }
+function runFactorial(n) {
+    console.log('run factorial')
+    return factorial(n)
+}
 const ComplexCalculateExample = () => {
     const [value, setValue] = useState(100)
+    const [otherState, setOtherState] = useState(false)
+
+    const buttonColor = otherState ? 'primary' : 'secondary'
     useEffect(() => {
-        console.log('render')
-    }, [value])
-    const fact = factorial(value)
+        console.log('render buttonColor')
+    }, [buttonColor])
+    // useMemo(() => runFactorial(value), [value])
+    const fact = useMemo(() => runFactorial(value), [value])
     return (
         <>
             <CardWrapper>
@@ -18,19 +26,24 @@ const ComplexCalculateExample = () => {
                 <p>Value: {value}</p>
                 <p>Result fact: {fact}</p>
                 <button
-                    className="btn btn-primary ms-md m-1"
+                    className="btn btn-primary mx-2"
                     onClick={() => setValue(prevState => prevState + 10)}>
                         increment
                 </button>
                 <button
-                    className="btn btn-primary ms-md m-1"
+                    className="btn btn-primary mx-2"
                     onClick={() => setValue(prevState => prevState - 10)}>
                         decrement
                 </button>
             </CardWrapper>
             <CardWrapper>
                 <SmallTitle>Зависимость от сторонних setState</SmallTitle>
-                <button className='btn btn-primary m-1'></button>
+                <button
+                    className={'btn ms-md-2 btn-' + buttonColor}
+                    onClick={() => setOtherState(prevState => !prevState)}
+                >
+                    жми, но не сильно
+                </button>
             </CardWrapper>
         </>
     )
